c4418f9e2eaa1e578d9a5055584242255e19cb5a
Gianni <BussolettiGianni@gmail.com>
2026-03-31 10:03:44 +0200

    float done

---
+++ float example/script.js
@@ -0,0 +1,9 @@
+const removeClear = () => {
+  const paragraph = document.getElementById("paragraphClear")
+  paragraph.classList.remove("clear-class")
+}
+
+const addClear = () => {
+  const paragraph = document.getElementById("paragraphClear")
+  paragraph.classList.add("clear-class")
+}
